89d8fed73917ee942735bee2c210d912d3488b20
sebastianriosanchez68@gmail.com <sebastianriosanchez68@gmail.com>
2019-04-06 01:30:35 -0400

    Commit Modelo MVC

---
+++ Parcial1/MVC/Models/Venta.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+
+
+    public enum TipoVenta
+    {
+        Alimentos,
+        Productos,
+        Bebidas
+    }
+    public enum TipoEstado
+    {
+        Activo,
+        Inactivo
+    }
+
+    public class Venta
+    {
+        [Key]
+        public int IdVenta { get; set; }
+
+        [Required]
+        public string Nombre  { get; set; }
+
+        [Required]
+        public decimal PrecioVenta { get; set; }
+
+        [Required]
+        public TipoVenta Ventas{ get; set; }
+
+        [Required]
+        public TipoEstado Estado { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
